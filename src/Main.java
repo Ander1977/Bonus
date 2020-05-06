16a0cc15b4b6a16944b4856b830656aa27c765f9
Andrei Kopatilov <koopa-andrej@live.com>
2020-05-06 15:39:30 +1000

    First

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 250;
+        int bonus = 1;
+        int refill = 2450;
+        if (refill > 1000) {
+            bonus = 1;
+        }
+        else { bonus = 0; }
+
+        
+
+        long totalbonus = refill * bonus / 100;
+
+        System.out.println(totalbonus);
+
+    }
+}
