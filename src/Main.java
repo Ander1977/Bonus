9e37d769d122304aa616f109dfdff8e6e9a8aaef
Andrei Kopatilov <koopa-andrej@live.com>
2020-05-06 16:57:19 +1000

    cod formatted

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+public
+class Main {
+    public static
+    void main(String[] args) {
         int balance = 250;
-        int bonus = 1;
-        int refill = 2450;
-        if (refill > 1000) { bonus = 1; }
-        else { bonus = 0; }
+        int bonus   = 1;
+        int refill  = 2450;
+        if (refill > 1000) {
+            bonus = 1;
+        }
+        else {
+            bonus = 0;
+        }
         long totalBonus = refill * bonus / 100;
-        System.out.println(totalBonus); }
+        System.out.println(totalBonus);
+    }
 }
